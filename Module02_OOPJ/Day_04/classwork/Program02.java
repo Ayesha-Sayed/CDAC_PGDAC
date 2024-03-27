43781b75de5497aa6d67654aebac7383f0704bb5
ClassGit <ClassGit@gmail.com>
2024-03-27 23:01:30 +0530

    created a demo program

---
+++ Module02_OOPJ/Day_04/classwork/Program02.java
@@ -0,0 +1,13 @@
+package classwork;
+
+
+	public class Program02 {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+	System.out.println("hiiiiii   ");
+		}
+
+	}
+
+
