e257b100e7f9f9a96d15d38a1522951d374e6bcd
ClassGit <ClassGit@gmail.com>
2024-04-20 23:14:15 +0530

    Created star pattern

---
+++ JAVA_ASSIGNMENT/src/star/pattern/star2.java
@@ -0,0 +1,24 @@
+package star.pattern;
+import java.util.*;
+
+class star2 {
+	
+//	    *
+//	   **
+//	  ***
+//	 ****
+//	*****
+	
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int i,j,k;
+		for(i = 1; i<= n; i++) {
+		for (j=4; j>=i; j--)
+			System.out.print(" ");	
+		for (k=1; k<= i; k++)
+			System.out.print("*");
+		System.out.println();	
+		}
+}}
