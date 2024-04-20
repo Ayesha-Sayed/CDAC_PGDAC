4b36c7bb96af85ebb87fee2f7e8bacf545c5a10a
ClassGit <ClassGit@gmail.com>
2024-04-21 01:26:12 +0530

    Update halfPyramid.java

--- JAVA_ASSIGNMENT/src/star/pattern/halfPyramid.java
+++ JAVA_ASSIGNMENT/src/star/pattern/halfPyramid.java
@@ -2,7 +2,14 @@ package star.pattern;
 import java.util.*;
 
 public class halfPyramid {
-
+	
+//	5
+//	*
+//	**
+//	***
+//	****
+//	*****
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
@@ -14,15 +21,6 @@ public class halfPyramid {
 			}
 			System.out.println();		
 			}
-		
-		for (int i=1; i<=n; i++)
-		{
-			for (int j=n; j>=i; j--)
-			{
-			     System.out.print("*");	
-			}
-			System.out.println();		
-			}
-		
-	}
+	
+}
 }
