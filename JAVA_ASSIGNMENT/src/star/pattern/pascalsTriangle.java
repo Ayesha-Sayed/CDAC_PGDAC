48684a8c008b4ae491b638321f50ec32c794d6ca
ClassGit <ClassGit@gmail.com>
2024-04-21 11:19:09 +0530

    Update pascalsTriangle.java

--- JAVA_ASSIGNMENT/src/star/pattern/pascalsTriangle.java
+++ JAVA_ASSIGNMENT/src/star/pattern/pascalsTriangle.java
@@ -8,7 +8,7 @@ import java.util.*;
 //1   4   6   4   1
 
 
-public class Pattern01 {
+public class pascalsTriangle {
 	public static void main(String[] args) {
 
 		Scanner sc=new Scanner(System.in);
